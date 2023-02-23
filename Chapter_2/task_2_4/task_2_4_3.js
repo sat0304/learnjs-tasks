187525c319630defb9f1e215d915a8e5548bfe81
Сергей Токарeв <sat0304@yandex.ru>
2023-02-23 23:39:38 +0400

    Date split is getted

--- Chapter_2/task_2_4/task_2_4_3.js
+++ Chapter_2/task_2_4/task_2_4_3.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function someCode(current_Date) {
-    let date =  new Date().getFullYear();
+    let yearCurrentdate =  new Date().getFullYear();
     let yearPastTime = current_Date.split('.')[2];
-    return date - yearPastTime;
+    return yearCurrentdate - yearPastTime;
 }
 
 const BIRTHDAY = '18.04.1982';
 const age = someCode(BIRTHDAY);
 
 
-// console.log(BIRTHDAY);
-// console.log(age);
+console.log(BIRTHDAY);
+console.log(age);
 
-alert(BIRTHDAY);
-alert(age);
+// alert(BIRTHDAY);
+// alert(age);
