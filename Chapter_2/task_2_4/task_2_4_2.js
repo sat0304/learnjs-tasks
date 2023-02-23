520b0e3ff8fb52e5551abd44d68ac5a88a0cfdea
Сергей Токарeв <sat0304@yandex.ru>
2023-02-23 23:01:22 +0400

    Long name is added

---
+++ Chapter_2/task_2_4/task_2_4_2.js
@@ -0,0 +1,7 @@
+'use strict';
+let ourPlanetEarth = 'Земля';
+let currentUserName = 'Marty McFly';
+// console.log(ourPlanetEarth);
+// console.log(currentUserName);
+alert(ourPlanetEarth);
+alert(currentUserName);
