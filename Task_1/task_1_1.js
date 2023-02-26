df3086b05646f56e1b44d2b0f2130947627827af
Сергей Токарeв <sat0304@yandex.ru>
2023-02-26 23:35:49 +0400

    It is the start of the task edit

---
+++ Task_1/task_1_1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let strRepair = {
+    
+  spaceExtractor() {
+    alert("Привет!");
+  }
+};
+  
+strRepair.spaceExtractor();
---
+++ Task_3/task_3_1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let str = "Привет";
+
+str.test = 5;
+
+alert( str.test );
+// console.log(str.test)
+// console.log(str?.[test])
