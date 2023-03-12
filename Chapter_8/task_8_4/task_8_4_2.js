3ff6a3bb4ce401ce46c2c43329bbe7aaef6bf93b
Сергей Токарeв <sat0304@yandex.ru>
2023-03-12 23:55:16 +0400

    prototype is added

--- Chapter_8/task_8_4/task_8_4_2.js
+++ Chapter_8/task_8_4/task_8_4_2.js
@@ -1,2 +1,16 @@
 'use strict';
 
+function Rabbit(name) {
+    this.name = name;
+  }
+  Rabbit.prototype.sayHi = function() {
+    alert( this.name );
+  }
+  
+  let rabbit = new Rabbit("Rabbit");
+  
+  rabbit.sayHi();                        // Rabbit
+  Rabbit.prototype.sayHi();              // undefined
+  Object.getPrototypeOf(rabbit).sayHi(); // undefined
+  rabbit.__proto__.sayHi();              // undefined
+  
